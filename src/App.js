8bb530f3582c94a225058374a1c48637d2f85b09
Karanb1 <Balodikanu124@gmail.com>
2021-09-02 03:02:15 +0530

    implemented hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./Screens/About/About";
 import Home from "./Screens/Home/Home";
 import Photography from "./Screens/Photography/Photography";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
