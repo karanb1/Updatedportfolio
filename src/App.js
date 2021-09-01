4af216bb020c3f73692696e309ef1a51c9ea5039
Karanb1 <Balodikanu124@gmail.com>
2021-09-02 03:11:04 +0530

    url switched

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Photography from "./Screens/Photography/Photography";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/">
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
